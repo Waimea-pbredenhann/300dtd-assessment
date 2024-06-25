5d5c9b48477c303c48a6b655e1e078ec045430c4
Waimea-pbredenhann <pbredenhann@waimea.school.nz>
2024-06-25 13:49:18 +1200

    closer

--- site/signup-form.php
+++ site/signup-form.php
@@ -1,49 +1,38 @@
-
 <?php require_once '_config.php'; ?>
-
 <?php require 'partials/top.php'; ?>
+<?php require 'partials/header.php'; ?>
 
-    <?php require 'partials/header.php'; ?>
-
-    <main>
-
+<main>
     <form method="post" action="process-signup.php">
-
         <div class="input-group">
-        <label>Username</label>
-        <input type="text" name="username" value="">
+            <label>Username</label>
+            <input type="text" name="username" value="" required>
         </div>
-
         <div class="input-group">
-        <label>Forename</label>
-        <input type="forename" name="forename" value="">
+            <label>Forename</label>
+            <input type="text" name="forename" value="" required>
         </div>
-
         <div class="input-group">
-        <label>Surname</label>
-        <input type="surname" name="surname" value="">
+            <label>Surname</label>
+            <input type="text" name="surname" value="" required>
         </div>
-
         <div class="input-group">
-        <label>Password</label>
-        <input type="password" name="password_1">
+            <label>Password</label>
+            <input type="password" name="password_1" required>
         </div>
-
         <div class="input-group">
-        <label>Confirm password</label>
-        <input type="password" name="password_2">
+            <label>Confirm password</label>
+            <input type="password" name="password_2" required>
         </div>
-
         <div class="input-group">
-        <button type="submit" class="btn" name="reg_user">Register</button>
+            <button type="submit" class="btn" name="reg_user">Register</button>
         </div>
         <p>
-        Already a member? <a href="login-form.php">Sign in</a>
+            Already a member? <a href="login-form.php">Sign in</a>
+            <a href="process-signup.php">-----</a>
         </p>
-        </form>
-    
-    </main>
+    </form>
+</main>
 
-    <?php require 'partials/footer.php'; ?>
-
+<?php require 'partials/footer.php'; ?>
 <?php require 'partials/bottom.php'; ?>
