ddeca5a5ee7aa5b00be5790d0dbf0b871205394d
Waimea-pbredenhann <pbredenhann@waimea.school.nz>
2024-07-02 12:17:14 +1200

    .

---
+++ site/user-view.php
@@ -0,0 +1,23 @@
+
+<?php require_once '_config.php'; ?>
+<?php require 'partials/top.php'; ?>
+<?php require 'partials/header.php'; ?>
+
+    <main>
+
+        <h1>Welcome to User's View</h1>
+    
+        <ul>
+            <li>Complete the view/list page</li>
+            <li>Use HTMX in view/list, creating list, and etc.</li>
+            <li>Link login/sign-up to page</li>
+            <li>Update development page</li>
+            <li>Deleting lists/views </li>
+            <li>check on js (optional)</li>
+            
+        </ul>
+
+    </main>
+
+<?php require 'partials/footer.php'; ?>
+<?php require 'partials/bottom.php'; ?>
